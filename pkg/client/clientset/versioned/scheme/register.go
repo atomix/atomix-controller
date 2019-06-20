4275528e4b504eeac437a3c02e32a58218109dfb
Jordan Halterman <jordan.halterman@gmail.com>
2019-06-19 17:57:37 -0700

    Fix compiler error in generated code.

--- pkg/client/clientset/versioned/scheme/register.go
+++ pkg/client/clientset/versioned/scheme/register.go
@@ -31,7 +31,7 @@ var Scheme = runtime.NewScheme()
 var Codecs = serializer.NewCodecFactory(Scheme)
 var ParameterCodec = runtime.NewParameterCodec(Scheme)
 var localSchemeBuilder = runtime.SchemeBuilder{
-	k8sv1alpha1.AddToScheme,
+	k8sv1alpha1.SchemeBuilder.AddToScheme,
 }
 
 // AddToScheme adds all types of this clientset into the given scheme. This allows composition
